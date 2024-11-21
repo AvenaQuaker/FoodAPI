fe29925ea46c2ee5f0573daabdcdd7ea57e6b019
Jaime Aquino Martinez <jhonesbortoni@gmail.com>
2024-11-21 10:18:12 -0600

    LE QUITE EL HTTPS POR AHORA

--- Server.js
+++ Server.js
@@ -30,11 +30,11 @@ app.use(
 );
 
 // Inicialización de Passport
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 //Servidor HTTPS
-const httpsServer = https.createServer(credentials, app);
+// const httpsServer = https.createServer(credentials, app);
 
 //Rutas Autentification
 app.use(AuthRouter());
@@ -42,20 +42,20 @@ app.use(AuthRouter());
 // Rutas principales
 app.use(crearRouter({ foodModel: FoodModel }));
 
-app.use("/Doc", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+// app.use("/Doc", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 //Obtener el objeto de Swagger
-app.get("/Spec", (req, res) => {
-    res.json(swaggerDocs);
-});
+// app.get("/Spec", (req, res) => {
+//     res.json(swaggerDocs);
+// });
 
 // Inicialización del servidor
 const PORT = process.env.PORT ?? 1234;
 
-httpsServer.listen(PORT, () => {
-    console.log(`Server is running on port https://localhost:${PORT}`);
-});
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port http://localhost:${PORT}`);
+// httpsServer.listen(PORT, () => {
+//     console.log(`Server is running on port https://localhost:${PORT}`);
 // });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
